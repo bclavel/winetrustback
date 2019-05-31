693d22321f60c11c9e05e791faa7301aff528d2a
Benoit Clavel <benoitclavel@yahoo.fr>
2019-05-31 16:11:29 +0200

    create product fix

--- routes/index.js
+++ routes/index.js
@@ -9,18 +9,22 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/createproduct', function(req, res, next) {
+router.post('/createproduct', function(req, res, next) {
+  console.log('ROUTE BACK - Create product req.body', req.body);
 
   var newProduct = new productModel({
+    productStatus : req.body.productStatus,
     productDomaine : req.body.productDomaine,
     productCuvee : req.body.productCuvee,
     productDeskImg : req.body.productDeskImg,
+    productYoutube : req.body.productYoutube,
     productMobImg : req.body.productMobImg,
     productMillesime : req.body.productMillesime,
     productCepages : req.body.productCepages,
     productAppellation : req.body.productAppellation,
     productRegion : req.body.productRegion,
     productCountry : req.body.productCountry,
+    productQuality: req.body.productQuality,
     domainHistory : req.body.domainHistory,
     productAccords : req.body.productAccords,
     domainPostalAddress : req.body.domainPostalAddress,
@@ -31,7 +35,7 @@ router.get('/createproduct', function(req, res, next) {
   newProduct.save(
     function (error, product) {
       console.log('INDEX BACK - New product save', product);
-      res.json({result : true, data});
+      res.json({result : true, product});
     });
 });
 
